37bc1086cd5647c67d1988793e002a3d19496029
gurk <gurkmandur@gmail.com>
2019-07-09 21:42:55 -0400

    Adding some mui forms

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
 //import { Router } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import FormControl from "@material-ui/core/FormControl";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
 
 import axios from "axios";
 
@@ -59,8 +64,7 @@ class Register extends Component {
     return (
       <div className={classes.landingContainer}>
         <NavBar location={this.props.location} />
-        <h1>Welcome to Twitter Audio</h1>
-        <h2> Sign up</h2>
+        <h1>Sign Up</h1>
         <form onSubmit={this.handleSubmit}>
           Name:
           <input
@@ -68,7 +72,7 @@ class Register extends Component {
             value={userName}
             name="userName"
             onChange={this.handleChange}
-          />{" "}
+          />
           <br />
           <br />
           Email:
@@ -77,11 +81,29 @@ class Register extends Component {
             value={userEmail}
             name="userEmail"
             onChange={this.handleChange}
-          />{" "}
+          />
           <br />
           <br />
           <input type="submit" value="Sign up" />
         </form>
+
+        <FormControl>
+          <InputLabel htmlFor="my-input">Email address</InputLabel>
+          <Input id="my-input" aria-describedby="my-helper-text" />
+          <FormHelperText id="my-helper-text">
+            We'll never share your email.
+          </FormHelperText>
+        </FormControl>
+
+        <FormControl className={classes.formControl} variant="outlined">
+          <InputLabel htmlFor="component-outlined">Full Name</InputLabel>
+          <OutlinedInput
+            id="component-outlined"
+            value={userName}
+            onChange={this.handleChange}
+            labelWidth={0}
+          />
+        </FormControl>
       </div>
     );
   }
