3588af95d3823bf9ebcbff387f4acf58ef72b6cc
gurk <gurkmandur@gmail.com>
2019-07-11 21:52:11 -0400

    cleaned up code, and updated styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
@@ -10,21 +11,66 @@ import logo from "../assets/logo.png";
 const useStyles = makeStyles(theme => ({
   root: {},
   logo: {
-    marginRight: theme.spacing(2),
-    height: "45px"
+    marginTop: theme.spacing(1),
+    height: "42px"
   },
   button: {
-    margin: theme.spacing(0.5),
+    margin: theme.spacing(1),
     textTransform: "none"
   },
   link: {
     textDecoration: "none"
+  },
+  notice: {
+    display: "inline-block",
+    fontWeight: "bold",
+    marginRight: theme.spacing(1)
+  },
+  appbar: {
+    boxShadow: "none",
+    borderBottom: "1px solid #E8E8E8"
+  },
+  toolbar: {
+    padding: "5px",
+    // center elements
+    right: "0",
+    left: "0",
+    marginRight: "auto",
+    marginLeft: "auto",
+    width: "95%"
   }
 }));
 
 function NavBar(props) {
   const classes = useStyles();
 
+  // navbar for register and login
+  function registerLoginNavLinks(pathname) {
+    // generate link for the opposite page
+    //if on register page, add link to login and vice versa
+    const oppositeLink = pathname === "/" ? "/login" : "/";
+
+    const buttonText = pathname === "/" ? "Log in" : "Sign up";
+    const noticeText =
+      pathname === "/" ? "Already a member?" : "Don't have an account?";
+
+    return (
+      <div>
+        <Typography className={classes.notice}>{noticeText}</Typography>
+        <Link to={oppositeLink} className={classes.link}>
+          <Button
+            variant="contained"
+            color="secondary"
+            className={classes.button}
+          >
+            {buttonText}
+          </Button>
+        </Link>
+      </div>
+    );
+  }
+
+  // navbar for when logged in
   function loggedInNavLinks() {
     return (
       <div>
@@ -67,20 +113,7 @@ function NavBar(props) {
 
     // currently in register or login page
     if (pathname === "/" || pathname === "/login") {
-      // generate link for the opposite page
-      const oppositeLink = pathname === "/" ? "/login" : "/";
-      const displayText = oppositeLink === "/" ? "Sign up" : "Log in";
-      return (
-        <Link to={oppositeLink} className={classes.link}>
-          <Button
-            variant="contained"
-            color="secondary"
-            className={classes.button}
-          >
-            {displayText}
-          </Button>
-        </Link>
-      );
+      return registerLoginNavLinks(pathname);
     }
     // else loggedin section!
     else {
@@ -90,9 +123,11 @@ function NavBar(props) {
 
   return (
     <div className={classes.root}>
-      <AppBar color="primary" position="static">
-        <Toolbar variant="regular">
-          <img src={logo} className={classes.logo} />
+      <AppBar color="primary" position="static" className={classes.appbar}>
+        <Toolbar variant="regular" className={classes.toolbar}>
+          <div style={{ flex: "1" }}>
+            <img src={logo} className={classes.logo} />
+          </div>
           <span>{generateNavBarLinks()}</span>
         </Toolbar>
       </AppBar>
