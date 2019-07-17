2c7fd4e9b0beffa3697b9e1b9c49de1b8ba02477
gurk <gurkmandur@gmail.com>
2019-07-17 19:39:35 -0400

    Added audio recording and playback feature

--- client/src/pages/CreateConversation.js
+++ client/src/pages/CreateConversation.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
 
-import NavBar from "../components/NavBar";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
-import { Typography, IconButton } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import { Button } from "@material-ui/core";
-import recordButton from "../assets/record-button.png";
+
+import NavBar from "../components/NavBar";
+import AudioRecord from "../components/AudioRecord";
 
 const conversationStyle = theme => ({
   root: {
@@ -19,15 +20,68 @@ const conversationStyle = theme => ({
   button: {
     marginTop: theme.spacing(5),
     textTransform: "none"
+  },
+  error: {
+    color: "red",
+    fontWeight: "bold",
+    textAlign: "center"
+    // backgroundColor: amber[700],
+    // border: "5px solid",
+    // borderColor: amber[700],
+    // borderRadius: "5px",
+    // marginTop: theme.spacing(2),
+    // paddingLeft: theme.spacing(2),
+    // paddingRight: theme.spacing(2)
   }
 });
 
 class CreateConversation extends Component {
-  state = {};
+  state = {
+    title: "",
+    blobObject: null,
+    errorMessage: null
+  };
+
+  handleTitleChange = e => {
+    this.setState({
+      title: e.target.value
+    });
+  };
+
+  // recieved from AudioRecord component once user has recorded audio
+  handleRecordedAudio = recordedBlob => {
+    console.log("recieved recorded blob =", recordedBlob);
+    this.setState({
+      blobObject: recordedBlob
+    });
+
+    // TODO
+    // Save audio to user and amazon S3
+  };
+
+  submitConversation = () => {
+    const { title, blobObject } = this.state;
+    // make sure title and audio has been set
+    if (title === "") {
+      this.setState({
+        errorMessage: "Please enter a title"
+      });
+    } else if (blobObject === null) {
+      this.setState({
+        errorMessage: "Please record audio"
+      });
+    } else {
+      // success!
+      console.log("Conversation created! (umm soon)");
+
+      // TODO
+      // Save audio to user and amazon S3
+    }
+  };
 
   render() {
     const { classes } = this.props;
-    console.log(this.props.location);
+    const { title, errorMessage } = this.state;
     return (
       <div>
         <NavBar location={this.props.location} />
@@ -42,38 +96,47 @@ class CreateConversation extends Component {
             <Typography variant="h5">Create a new conversation</Typography>
           </Grid>
 
-          <Grid item xs={12} className={classes.item}>
+          <Grid item className={classes.item}>
             <TextField
               id="title"
               name="title"
               placeholder="Enter Title here"
               margin="normal"
+              autoComplete="off"
               fullWidth
+              value={title}
+              onChange={this.handleTitleChange}
               inputProps={{ style: { textAlign: "center" } }}
               style={{ width: "500px" }}
             />
           </Grid>
 
-          <Grid item className={classes.item}>
-            <IconButton color="secondary">
-              <img src={recordButton} alt="record" />
-            </IconButton>
+          <Grid item>
+            <AudioRecord onRecordAudio={this.handleRecordedAudio} />
           </Grid>
-          <Grid item className={classes.item}>
+          {/* <Grid item className={classes.item}>
             <Typography>press to start recording</Typography>
           </Grid>
           <Grid item>
             <Typography>0:00</Typography>
-          </Grid>
+          </Grid> */}
           <Grid item>
             <Button
               variant="contained"
               color="secondary"
               className={classes.button}
               type="submit"
+              onClick={this.submitConversation}
             >
               Create Conversation
             </Button>
+            <Grid item className={classes.item}>
+              {errorMessage ? (
+                <div className={classes.error}>{errorMessage}</div>
+              ) : (
+                ""
+              )}
+            </Grid>
           </Grid>
         </Grid>
       </div>
