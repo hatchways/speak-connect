2d52e2035147abe0d206eb7f157ef7f61e243b14
Alain Davis <davisbedetse@gmail.com>
2019-07-18 19:00:05 -0400

    pic upload fixed.aws credentials stored in config

--- server/services/fileUpload.js
+++ server/services/fileUpload.js
@@ -1,11 +1,12 @@
 const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
+const config = require("config");
+
 
 aws.config.update({
-    secretAccessKey:
-        "1pu4+1iTbeOVODJKHkTi9wyeqmQYAce7QM9RRBra",
-    accessKeyId: "AKIAIVSW3VQEYJH3LNDA",
+    secretAccessKey: config.get("secretAccessKey"),
+    accessKeyId: config.get("accessKeyId"),
     region: "us-east-2",
 })
 
