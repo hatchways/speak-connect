942b2c40973dc50f88b67f180d8bd648dda8c2c8
gurk <gurkmandur@gmail.com>
2019-07-26 12:49:38 -0400

    set feed styling

--- client/src/components/FeedUser.js
+++ client/src/components/FeedUser.js
@@ -2,57 +2,133 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
+import Divider from "@material-ui/core/Divider";
 
+import ThumbUp from "@material-ui/icons/ThumbUp";
+import ChatBubble from "@material-ui/icons/ChatBubble";
+import Reply from "@material-ui/icons/Reply";
+import Star from "@material-ui/icons/Star";
+
+import { StyledButton } from "../themes/theme";
 import AudioPlayer from "./AudioPlayer";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    width: "700px",
+    width: "800px",
     height: "500px",
     boxShadow: "0px 0px 30px 5px #f0f0f0",
     borderRadius: "5px",
-    paddingTop: theme.spacing(3)
+    paddingTop: theme.spacing(3),
+    paddingLeft: theme.spacing(5)
   },
-  item: {
-    margin: theme.spacing(1),
-    align: "center",
-    textAlign: "center"
-  },
-  primaryText: {
-    fontSize: "24px",
+  nameText: {
     fontWeight: "bold"
   },
-  secondaryText: {
-    color: "#adadad" // grey
+  title: {
+    fontSize: "28px",
+    fontWeight: "bold",
+    marginBottom: theme.spacing(1),
+    marginTop: theme.spacing(3)
+  },
+  container: {
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4),
+    width: "90%"
+  },
+  item: {
+    marginRight: theme.spacing(2)
+  },
+  icon: {
+    color: "#dfe3f0", // blue grey
+    marginRight: theme.spacing(1)
+    // position: "relative",
+    // top: "5px",
+    // paddingRight: theme.spacing(1)
+  },
+  text: {
+    fontWeight: "bold"
+  },
+  button: {
+    color: "black"
+  },
+  divider: {
+    // make divider same size as panel
+    position: "relative",
+    left: "-40px",
+    width: "840px"
   }
 }));
 
 function FeedUser(props) {
   const classes = useStyles();
   const { name, title, audioURL } = props;
-  console.log("name  =", name);
+
+  const generateHeader = () => {
+    return (
+      <div>
+        <Typography className={classes.nameText}>{name}</Typography>
+        <Typography className={classes.title}>{title}</Typography>
+        <AudioPlayer audioURL={audioURL} />
+        <Grid container alignItems="center" className={classes.container}>
+          <Grid item>
+            <ThumbUp className={classes.icon} />
+          </Grid>
+          <Grid item id="likes" className={classes.item}>
+            <Typography className={classes.text}>0</Typography>
+          </Grid>
+
+          <Grid item>
+            <ChatBubble className={classes.icon} />
+          </Grid>
+          <Grid item id="comments" className={classes.item}>
+            <Typography className={classes.text}>0</Typography>
+          </Grid>
+
+          <Grid item id="reply" className={classes.item}>
+            <StyledButton className={classes.button} variant="outlined">
+              <Reply style={{ color: "#dfe3f0" }} />
+              <Typography className={classes.text} component="span">
+                Reply
+              </Typography>
+            </StyledButton>
+          </Grid>
+
+          <Grid item id="follow" className={classes.item}>
+            <StyledButton className={classes.button} variant="outlined">
+              <Star style={{ color: "#dfe3f0" }} />
+              <Typography className={classes.text} component="span">
+                Follow
+              </Typography>
+            </StyledButton>
+          </Grid>
+
+          <Grid item style={{ marginRight: "10px", flex: 1 }}>
+            <Typography
+              className={classes.text}
+              style={{
+                float: "right",
+                color: "#adadad" // grey
+              }}
+              component="span"
+            >
+              Listens:
+            </Typography>
+          </Grid>
+          <Grid item id="listens" className={classes.item}>
+            <Typography className={classes.text} component="span">
+              0
+            </Typography>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  };
 
   return (
-    <Grid
-      container
-      direction="column"
-      className={classes.root}
-      alignItems="center"
-    >
-      <Grid item id="name" className={classes.item}>
-        <Typography className={classes.primaryText}>{name}</Typography>
-      </Grid>
-
-      <Grid item id="title" className={classes.item}>
-        <Typography className={classes.primaryText}>{title}</Typography>
-      </Grid>
-
-      <Grid item id="audio" className={classes.item}>
-        <div className={classes.player}>
-          <AudioPlayer audioURL={audioURL} />
-        </div>
-      </Grid>
-    </Grid>
+    <div className={classes.root}>
+      {generateHeader()}
+      <Divider className={classes.divider} />
+    </div>
   );
 }
 
--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -1,41 +1,78 @@
 import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { Grid, Container } from "@material-ui/core";
+
 import FeedUser from "../components/FeedUser";
-import { Grid } from "@material-ui/core";
+import NavBar from "../components/NavBar";
 
 import axios from "axios";
 
+const useStyles = theme => ({
+  container: {
+    marginTop: theme.spacing(5)
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: "28px"
+  },
+  item: {
+    marginBottom: theme.spacing(2)
+  },
+  button: {
+    marginTop: theme.spacing(5)
+  },
+  error: {
+    color: "red",
+    fontWeight: "bold",
+    textAlign: "center"
+  }
+});
+
 class Feed extends Component {
-    state = {
-        conversations: []
-    };
+  state = {
+    conversations: []
+  };
 
-    async componentDidMount() {
-        await axios
-            .get(`/api/users/conversations`)
-            .then(response => {
-                console.log("conversations feed:", response.data);
-                this.setState({
-                    conversations: response.data,
-                });
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
+  async componentDidMount() {
+    await axios
+      .get(`/api/users/conversations`)
+      .then(response => {
+        console.log("conversations feed:", response.data);
+        this.setState({
+          conversations: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-    render() {
-        return (
-            this.state.conversations.map(conversation => (
-                <Grid item key={conversation._id} >
-                    <FeedUser
-                        name={conversation.userName}
-                        title={conversation.title}
-                        audioURL={conversation.audio}
-                    />
-                </Grid>
-            ))
-        )
-    }
+  generateConversations = classes => {
+    const posts = this.state.conversations.map(conversation => (
+      <Grid item key={conversation._id} className={classes.item}>
+        <FeedUser
+          name={conversation.userName}
+          title={conversation.title}
+          audioURL={conversation.audio}
+        />
+      </Grid>
+    ));
+    return posts;
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <NavBar location={this.props.location} />
+        <Container className={classes.container}>
+          <Grid container className={classes.grid}>
+            {this.generateConversations(classes)}
+          </Grid>
+        </Container>
+      </div>
+    );
+  }
 }
 
-export default Feed;
+export default withStyles(useStyles)(Feed);
