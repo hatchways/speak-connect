b65850bd7f73f671f1553ee860e0776e4b405514
gurk <gurkmandur@gmail.com>
2019-07-30 17:55:51 -0400

    Display username and profile pic in the convo dialog

--- client/src/components/ConversationDialog.js
+++ client/src/components/ConversationDialog.js
@@ -3,33 +3,40 @@ import { makeStyles } from "@material-ui/core/styles";
 
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
-import { Container, Paper, Typography, Grid } from "@material-ui/core";
+import { Container, Typography, Grid } from "@material-ui/core";
 import Divider from "@material-ui/core/Divider";
 
 import ThumbUp from "@material-ui/icons/ThumbUp";
 import ChatBubble from "@material-ui/icons/ChatBubble";
-import Reply from "@material-ui/icons/Reply";
 import Star from "@material-ui/icons/Star";
+import defaultProfilePic from "../assets/default-profile-pic.png";
 
 import { StyledButton } from "../themes/theme";
 import AudioPlayer from "./AudioPlayer";
+import ReplyDialog from "./ReplyDialog";
 
 const useStyles = makeStyles(theme => ({
   dialog: {
     background: "rgb(57, 86, 225, 0.9)" // blue
     // background: "rgb(240, 240, 240, 0.9)" // grey
   },
-  root: {
-    marginTop: theme.spacing(5)
-  },
+  root: {},
   title: {
     fontSize: "28px",
     fontWeight: "bold",
     marginBottom: theme.spacing(1),
-    marginTop: theme.spacing(3)
+    marginTop: theme.spacing(2)
+  },
+  primaryText: {
+    fontWeight: "bold"
+  },
+  secondaryText: {
+    fontSize: "14px",
+    fontWeight: "bold",
+    color: "#adadad" // grey
   },
   container: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(3),
     marginBottom: theme.spacing(4),
     width: "90%"
   },
@@ -54,12 +61,24 @@ const useStyles = makeStyles(theme => ({
     position: "relative",
     left: "-24px",
     width: "50vw"
+  },
+  profilePicture: {
+    width: "100%",
+    height: "100%",
+    maxWidth: "50px",
+    maxHeight: "50px",
+    minWidth: "50px",
+    minHeight: "50px",
+
+    objectFit: "cover",
+    borderRadius: "50%",
+    marginRight: theme.spacing(2)
   }
 }));
 
 function ConversationDialog(props) {
   const classes = useStyles();
-  const { title, audioURL, open } = props;
+  const { title, audioURL, name, username, imageUrl, open } = props;
 
   return (
     <div>
@@ -83,8 +102,26 @@ function ConversationDialog(props) {
       >
         <DialogContent>
           <Container className={classes.root}>
+            <Grid container alignItems="center" style={{ marginTop: "20px" }}>
+              <Grid item id="profilePicture">
+                <img
+                  src={imageUrl ? imageUrl : defaultProfilePic}
+                  className={classes.profilePicture}
+                  alt="Profile pic"
+                />
+              </Grid>
+              <Grid item id="name" style={{ marginRight: "8px" }}>
+                <Typography className={classes.primaryText}>{name}</Typography>
+              </Grid>
+              <Grid item id="username">
+                <Typography className={classes.secondaryText}>
+                  @{username}
+                </Typography>
+              </Grid>
+            </Grid>
             <Typography className={classes.title}>{title}</Typography>
             <AudioPlayer audioURL={audioURL} />
+
             <Grid container alignItems="center" className={classes.container}>
               <Grid item>
                 <ThumbUp className={classes.icon} />
