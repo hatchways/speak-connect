b2972e42ecf583b75d31061d3e7363160d01b47e
alaindave <48536549+alaindave@users.noreply.github.com>
2019-09-14 16:01:33 -0400

    Update fileUpload.js

--- services/fileUpload.js
+++ services/fileUpload.js
@@ -13,7 +13,7 @@ const s3 = new aws.S3();
 const upload = multer({
 	storage: multerS3({
 		s3,
-		bucket: 'textbook-bucket',
+		bucket: 'speak-app-bucket',
 		acl: 'public-read',
 		metadata: function(req, file, cb) {
 			cb(null, { fieldName: file.fieldname });
