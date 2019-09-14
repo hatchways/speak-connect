016d942559c54a220766fac06f7940b4574bd19f
Alain <davisbedetse@gmail.com>
2019-09-14 11:19:34 -0400

    config fix

--- app.js
+++ app.js
@@ -1,54 +1,50 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const dotenv = require("dotenv");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv');
 dotenv.config();
 
-const userRouter = require("./routes/userRoute");
-const loginRouter = require("./routes/loginRoute");
+const userRouter = require('./routes/userRoute');
+const loginRouter = require('./routes/loginRoute');
 
-if (
-  !process.env.jwtKey ||
-  !process.env.secretAccessKey ||
-  !process.env.accessKeyId
-) {
-  console.error("FATAL ERROR:ENV VARIABLES NOT SET!");
-  process.exit(1);
+if (!process.env.jwtKey || !process.env.secretAccessKey || !process.env.accessKeyId || !process.env.MONGODB_URI) {
+	console.error('FATAL ERROR:ENV VARIABLES NOT SET!');
+	process.exit(1);
 }
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.use("/api/users", userRouter);
-app.use("/api/auth", loginRouter);
+app.use('/api/users', userRouter);
+app.use('/api/auth', loginRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({ error: err });
-  console.log(err);
+	// render the error page
+	res.status(err.status || 500);
+	res.json({ error: err });
+	console.log(err);
 });
 
 module.exports = app;
--- db.js
+++ db.js
@@ -3,180 +3,165 @@ const Users = require('./models/userModel');
 const Conversation = require('./models/conversationModel');
 const Comments = require('./models/commentsModel');
 
-
-mongoose.connect('mongodb://localhost/profiledb', { useNewUrlParser: true })
-    .then(() => console.log('Connected to MongoDb...'))
-    .catch(err => console.error('Could not connect to MongoDB', err))
+mongoose
+	.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+	.then(() => console.log('Connected to MongoDb...'))
+	.catch((err) => console.error('Could not connect to MongoDB', err));
 
 const addUser = async (_user) => {
-    const user = new Users({
-        name: _user.name,
-        username: _user.username,
-        email: _user.email,
-        password: _user.password,
-        location: "",
-        description: "",
-        imageUrl: ""
-    });
+	const user = new Users({
+		name: _user.name,
+		username: _user.username,
+		email: _user.email,
+		password: _user.password,
+		location: '',
+		description: '',
+		imageUrl: ''
+	});
 
-    try {
-        const result = await user.save();
-        return result;
-    }
-    catch (e) {
-        console.log('unable to register user')
-        return e.message
-    }
-}
-
+	try {
+		const result = await user.save();
+		return result;
+	} catch (e) {
+		console.log('unable to register user');
+		return e.message;
+	}
+};
 
 const addPic = async (url, id) => {
-    try {
-        // Fetch user with the given id
-        const user = await Users.findById(id).populate("conversations");
+	try {
+		// Fetch user with the given id
+		const user = await Users.findById(id).populate('conversations');
 
-        //add url to user document
-        user.imageUrl = url;
+		//add url to user document
+		user.imageUrl = url;
 
-        //add url to conversation documents
-        const conversations = user.conversations;
-        conversations.forEach(function (conversation) {
-            conversation.imageUrl = url;
-            conversation.save();
-        });
-        // will need to do it for replies as well
+		//add url to conversation documents
+		const conversations = user.conversations;
+		conversations.forEach(function(conversation) {
+			conversation.imageUrl = url;
+			conversation.save();
+		});
+		// will need to do it for replies as well
 
-        user.save();
-        console.log('updated user', user);
-    }
-    catch (e) {
-        console.log('Unable to add image.Error message:', e.message);
-    }
-}
+		user.save();
+		console.log('updated user', user);
+	} catch (e) {
+		console.log('Unable to add image.Error message:', e.message);
+	}
+};
 
 const saveConvo = async (title, audio, userId) => {
+	// Fetch user with the given id
+	const user = await Users.findById(userId);
+	const name = user.name;
+	const username = user.username;
+	const imageUrl = user.imageUrl;
+	const userID = userId;
+	const userLikeMap = new Map();
 
-    // Fetch user with the given id
-    const user = await Users.findById(userId);
-    const name = user.name;
-    const username = user.username;
-    const imageUrl = user.imageUrl;
-    const userID = userId;
-    const userLikeMap = new Map();
-
-    const convo = new Conversation({
-        title,
-        audio,
-        name,
-        username,
-        imageUrl,
-        userID,
-        userLikeMap
-    });
-    try {
-        const result = await convo.save();
-        return result;
-    }
-    catch (e) {
-        console.log('Unable to save convo.', e);
-    }
-}
+	const convo = new Conversation({
+		title,
+		audio,
+		name,
+		username,
+		imageUrl,
+		userID,
+		userLikeMap
+	});
+	try {
+		const result = await convo.save();
+		return result;
+	} catch (e) {
+		console.log('Unable to save convo.', e);
+	}
+};
 
 const addConvo = async (userId, convoId) => {
-
-    try {
-        // Fetch user with the given id
-        const user = await Users.findById(userId);
-        user.conversations.push(convoId);
-        const result = await user.save();
-        return result;
-    }
-    catch (e) {
-        console.log('Unable to add convo to user document.Error message:', e);
-    }
-}
+	try {
+		// Fetch user with the given id
+		const user = await Users.findById(userId);
+		user.conversations.push(convoId);
+		const result = await user.save();
+		return result;
+	} catch (e) {
+		console.log('Unable to add convo to user document.Error message:', e);
+	}
+};
 
 // add or remove the user from the conversation like mapping
 const likeUnlikeConvo = async (userId, convoId) => {
-    try {
-        // Fetch conversation
-        const convo = await Conversation.findById(convoId);
+	try {
+		// Fetch conversation
+		const convo = await Conversation.findById(convoId);
 
-        // if user has alredy liked convo, then unlike it
-        if (convo.userLikeMap.get(userId)) {
-            convo.userLikeMap.delete(userId);
-        }
-        // like convo
-        else {
-            convo.userLikeMap.set(userId, true);
-        }
-        const result = await convo.save();
-        return result;
-    }
-    catch (e) {
-        console.log('Unable to like/unlke the conversation.Error message:', e);
-    }
-}
+		// if user has alredy liked convo, then unlike it
+		if (convo.userLikeMap.get(userId)) {
+			convo.userLikeMap.delete(userId);
+		} else {
+			// like convo
+			convo.userLikeMap.set(userId, true);
+		}
+		const result = await convo.save();
+		return result;
+	} catch (e) {
+		console.log('Unable to like/unlke the conversation.Error message:', e);
+	}
+};
 
 const getConversations = async () => {
-    try {
-        const conversations = await Conversation.find().
-            populate({
-                path: 'comments',
-                // populate author of comments
-                populate: {
-                    path: 'author',
-                    // only fetch fields we need 
-                    select: { name: '1', username: '1', imageUrl: '1' }
-                }
-            });
-        return conversations;
-    }
-    catch (e) {
-        console.log('unable to fetch conversations');
-        return e.message;
-    }
-}
+	try {
+		const conversations = await Conversation.find().populate({
+			path: 'comments',
+			// populate author of comments
+			populate: {
+				path: 'author',
+				// only fetch fields we need
+				select: { name: '1', username: '1', imageUrl: '1' }
+			}
+		});
+		return conversations;
+	} catch (e) {
+		console.log('unable to fetch conversations');
+		return e.message;
+	}
+};
 
 const saveComment = async (author, audio) => {
-
-    const date = new Date();
-    const comment = new Comments({
-        author,
-        audio,
-        date
-    });
-    try {
-        const result = await comment.save();
-        return result;
-    }
-    catch (e) {
-        console.log('Unable to save comment.', e);
-    }
-}
+	const date = new Date();
+	const comment = new Comments({
+		author,
+		audio,
+		date
+	});
+	try {
+		const result = await comment.save();
+		return result;
+	} catch (e) {
+		console.log('Unable to save comment.', e);
+	}
+};
 
 const addComment = async (convoId, commentID) => {
-
-    try {
-        // Fetch conversation with the given id
-        const conversation = await Conversation.findById(convoId);
-        conversation.comments.push(commentID);
-        const updated = await conversation.save();
-        console.log('Updated conversation', updated)
-        return updated;
-    }
-    catch (e) {
-        console.log('Unable to add new comment to conversation document', e);
-    }
-}
+	try {
+		// Fetch conversation with the given id
+		const conversation = await Conversation.findById(convoId);
+		conversation.comments.push(commentID);
+		const updated = await conversation.save();
+		console.log('Updated conversation', updated);
+		return updated;
+	} catch (e) {
+		console.log('Unable to add new comment to conversation document', e);
+	}
+};
 
 module.exports = {
-    addUser,
-    addPic,
-    saveConvo,
-    addConvo,
-    likeUnlikeConvo,
-    getConversations,
-    saveComment,
-    addComment
-}
+	addUser,
+	addPic,
+	saveConvo,
+	addConvo,
+	likeUnlikeConvo,
+	getConversations,
+	saveComment,
+	addComment
+};
